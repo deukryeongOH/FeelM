0219d32df099c7fbefcdafe0f5550b24f28963ce
deukr <deukryeongoh@gmail.com>
2025-11-13 11:53:59 +0900

    [CHORE] Project Setting

---
+++ src/main/java/project/feelm/FeelmApplication.java
@@ -0,0 +1,13 @@
+package project.feelm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/feelm/FeelmApplicationTests.java
@@ -0,0 +1,13 @@
+package project.feelm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

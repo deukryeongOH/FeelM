8698e106a6360e26e6cb835a9cea6cda1958afaa
deukryeongOH <ekdh0630@naver.com>
2025-12-05 18:04:31 +0900

    [FEAT] SpringBatchConfig with Schedular

--- Feelm-Backend/src/main/java/project/feelm/FeelmApplication.java
+++ Feelm-Backend/src/main/java/project/feelm/FeelmApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@EnableScheduling // 스케줄링 기능 활성화 (운영 시)
+@EnableScheduling // 스케줄링 기능 활성화 (배포 후 운영 시)
 @SpringBootApplication
 public class FeelmApplication {
 	public static void main(String[] args) {
